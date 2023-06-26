a133c38b1eeffd5a8c41fd2011d8de007eda69c3
Diego <dflores7237@gmail.com>
2023-06-26 14:23:25 -0700

    Added favicon. Updated project details and links. Deleted some unused images and references to old images.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ProjectComp from './ProjectComp';
 import vstdaImage from './assets/images/vstdaImage.png'
 import breweryFinderImage from './assets/images/breweryFinderImage.png'
 import portfolioImage from './assets/images/portfolioImage.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
@@ -30,31 +26,31 @@ function App() {
                   realized I could work on solving problems every day! I find myself driven by troubleshooting 
                   and the programming cycle of getting stuck, learning something new, and pushing on to the next 
                   road block.
-                  I'm happily married to my high school sweetheart and we have two young boys. Currently my free time is spent making memories with my family and traveling locally.
+                  I'm happily married to my high school sweetheart and we have three young kids. Currently my free time is spent making memories with my family and traveling locally.
                   I would like to work for a company that values team work and provides a positive environment for growth.</div>
           </div>
           <ProjectComp
               image={breweryFinderImage}
               title = 'Brewery Finder'
-              buildTools = 'React || Express'
+              buildTools = 'React'
               description = 'An app that uses your ip address to find any local breweries.'
-              githubLink = 'https://github.com/Dflo88/react-100-brewery-finder'
-              deployLink = 'https://dflores7237-brewery-finder.herokuapp.com/'
+              githubLink = 'https://github.com/Dflo88/serverless-brewery-finder'
+              deployLink = 'https://brewery-finder-7237.netlify.app'
           />
           <ProjectComp
               image={vstdaImage}
               title = 'To Do App'
               buildTools = 'React || Bootstrap'
               description = 'Simple todo app to manage what you need... to do.'
-              githubLink = 'https://github.com/Dflo88/VerySimpleToDoApp'
-              deployLink = 'https://dflores7237-vstda.herokuapp.com/'
+              githubLink = 'https://github.com/Dflo88/serverless-vstda'
+              deployLink = 'https://dvstda.netlify.app'
           />
           <ProjectComp
               image={portfolioImage}
               title = 'Portfolio'
-              buildTools = 'React || Heroku '
-              description = 'Clean portfolio created with React and deployed with Heroku'
-              githubLink = 'https://github.com/Dflo88/Portfolio'
+              buildTools = 'React || Netlify '
+              description = 'Clean portfolio created with React and deployed with Netlify'
+              githubLink = 'https://github.com/Dflo88/vite-portfolio'
           />
           <div className='contactInfo'>Lets Talk! &nbsp;
               <a className='contactInfoEmail' href="mailto:DFlores@gmail.com">DFlores7237@gmail.com</a>
