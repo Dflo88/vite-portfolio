3e5cb17c0a43e92271905e24672fc9a8ebc6eef5
Diego <dflores7237@gmail.com>
2023-06-26 17:08:18 -0700

    added logic so a mobile version of the project component will render for smaller viewport. Added styles to new component.

--- src/MobileProjectComp.jsx
+++ src/MobileProjectComp.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function MobileProjectComp({image, title, description, githubLink = "", deployLink = ""}) {
   
   return (
-    <div className="card projectCardStyle" >
-      <img src={image} className="card-img-top" alt="..."/>
+    <div className="card mobileCard projectCardStyle" >
+      <div className="mobileImageWrapper">
+        <img src={image} className="card-img-top" alt={`${title} app screenshot`}/>
+      </div>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        {deployLink && <a href={deployLink} className="card-link" target="_blank"><i className="bi bi-git"></i></a>}
-        {githubLink && <a href={githubLink} className="card-link" target="_blank"><i className="bi bi-box-arrow-up-right"></i></a>}
+        {deployLink && <a href={deployLink} className="btn mobileLinkBtn" target="_blank"><i className="bi bi-box-arrow-up-right"></i></a>}
+        {githubLink && <a href={githubLink} className="btn mobileLinkBtn" target="_blank"><i className="bi bi-git"></i></a>}
       </div>
     </div>
   )
